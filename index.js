da7fef16489fece594a210861257674891068415
Zac <guestyonline@gmail.com>
2021-03-15 10:30:29 +1100

    add structure and comments to index.html and index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
+// Hello API - ensure website is running 
+
+// Sum API - return the sum of the supplied integers. Not JSON formatted
+
+//Reverse Words API - reverse each word in supplied string and return result
 
