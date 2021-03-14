a6b5867bc33fcf44700f9b8d9077f12e2569d502
Zac <guestyonline@gmail.com>
2021-03-15 10:43:38 +1100

    add get from the instructions to index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Hello API - ensure website is running 
 
 // Sum API - return the sum of the supplied integers. Not JSON formatted
-
-//Reverse Words API - reverse each word in supplied string and return result
-
+GET /sum?numbers=<command_seperated_numbers>
+    
+{/* Reverse Words API - reverse each word in supplied string and return result */}
+GET /reverse-words?sentence=<a_string> 
